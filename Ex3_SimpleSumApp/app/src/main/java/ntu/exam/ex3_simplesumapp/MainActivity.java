4b720890e33d52f07dc017e4374d35a3a92d6f08
Nhật Trung Nguyễn <kenkenhallo16@gmail.com>
2025-02-24 15:24:06 +0700

    Xử lý backend

--- Ex3_SimpleSumApp/app/src/main/java/ntu/exam/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/ntu/exam/ex3_simplesumapp/MainActivity.java
@@ -1,6 +1,8 @@
 package ntu.exam.ex3_simplesumapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        //gắn layout tương ứng với file
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    //Bộ lắng nghe và xử lý sự kiện click lên nút tính tổng
+    public void XuLyCong(View view){
+        //tìm, tham chiếu đến điều khiển trên tệp XML
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKQ = findViewById(R.id.edtKqua);
+        //Lấy data về
+        String strA = editTextSoA.getText().toString();
+        String strB = editTextSoB.getText().toString();
+        //Chuyển data sang dạng số
+        int soA=Integer.parseInt(strA);
+        int soB=Integer.parseInt(strB);
+        //Tính toán theo y'c
+        int tong = soA + soB;
+        String strTong = String.valueOf(tong); //chuyển kqua sang chuỗi
+
+        //Hiện ra màn hình kqua
+
+        editTextKQ.setText(strTong);
+
     }
 }
