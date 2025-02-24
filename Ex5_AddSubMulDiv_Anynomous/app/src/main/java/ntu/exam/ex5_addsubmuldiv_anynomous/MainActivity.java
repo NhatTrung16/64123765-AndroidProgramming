7912a747220d911c0b9808befa48dfc82e8d80ce
Nhật Trung Nguyễn <kenkenhallo16@gmail.com>
2025-02-24 16:38:40 +0700

    Sử dụng bộ lắng nghe ẩn dan cho app máy tính

---
+++ Ex5_AddSubMulDiv_Anynomous/app/src/main/java/ntu/exam/ex5_addsubmuldiv_anynomous/MainActivity.java
@@ -0,0 +1,140 @@
+package ntu.exam.ex5_addsubmuldiv_anynomous;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Khai báo những đối tượng tìm kiếm
+    EditText so1;
+    EditText so2;
+    EditText kqua;
+    Button nutCong, nutTru, nutNhan, nutChia;
+    void TimDieuKhien(){
+        so1 = (EditText)findViewById(R.id.edtSo1);
+        so2 = (EditText)findViewById(R.id.edtSo2);
+        kqua = (EditText)findViewById(R.id.edtKQ);
+        nutCong = (Button)findViewById(R.id.btnCong);
+        nutTru = (Button)findViewById(R.id.btnTru);
+        nutNhan = (Button)findViewById(R.id.btnNhan);
+        nutChia = (Button)findViewById(R.id.btnChia);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TimDieuKhien();
+        //Gắn bộ lắng nghe sk và code xử lý cho từng nút
+        nutCong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //b1 lấy data từ 2 số
+//        EditText so1 = (EditText)findViewById(R.id.edtSo1);
+//        EditText so2 = (EditText)findViewById(R.id.edtSo2);
+                //b1.1 lấy data từ 2 điều khiển trên
+                String strso1 =so1.getText().toString();
+                String strso2 =so2.getText().toString();
+                //b1.3 chuyển data từ chuỗi sang số
+                float fso1 = Float.parseFloat(strso1);
+                float fso2 = Float.parseFloat(strso2);
+
+                //b2 tính toán
+                float Tong = fso1 + fso2;
+                //b3 hiện kết quả
+                // tìm data
+//        EditText kqua = (EditText)findViewById(R.id.edtKQ);
+                // cbi data xuất, biến thành dạng chuỗi
+                String strkqua = String.valueOf(Tong);
+                //gắn kqua lên đk
+                kqua.setText(strkqua);
+            }
+        });
+        nutTru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //b1 lấy data từ 2 số
+//        EditText so1 = (EditText)findViewById(R.id.edtSo1);
+//        EditText so2 = (EditText)findViewById(R.id.edtSo2);
+                //b1.1 lấy data từ 2 điều khiển trên
+                String strso1 =so1.getText().toString();
+                String strso2 =so2.getText().toString();
+                //b1.3 chuyển data từ chuỗi sang số
+                float fso1 = Float.parseFloat(strso1);
+                float fso2 = Float.parseFloat(strso2);
+
+                //b2 tính toán
+                float Hieu = fso1 - fso2;
+                //b3 hiện kết quả
+                // tìm data
+//        EditText kqua = (EditText)findViewById(R.id.edtKQ);
+                // cbi data xuất, biến thành dạng chuỗi
+                String strkqua = String.valueOf(Hieu);
+                //gắn kqua lên đk
+                kqua.setText(strkqua);
+            }
+        });
+        nutNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //b1 lấy data từ 2 số
+//        EditText so1 = (EditText)findViewById(R.id.edtSo1);
+//        EditText so2 = (EditText)findViewById(R.id.edtSo2);
+                //b1.1 lấy data từ 2 điều khiển trên
+                String strso1 =so1.getText().toString();
+                String strso2 =so2.getText().toString();
+                //b1.3 chuyển data từ chuỗi sang số
+                float fso1 = Float.parseFloat(strso1);
+                float fso2 = Float.parseFloat(strso2);
+
+                //b2 tính toán
+                float Tich = fso1 * fso2;
+                //b3 hiện kết quả
+                // tìm data
+//        EditText kqua = (EditText)findViewById(R.id.edtKQ);
+                // cbi data xuất, biến thành dạng chuỗi
+                String strkqua = String.valueOf(Tich);
+                //gắn kqua lên đk
+                kqua.setText(strkqua);
+            }
+        });
+        nutChia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //b1 lấy data từ 2 số
+//        EditText so1 = (EditText)findViewById(R.id.edtSo1);
+//        EditText so2 = (EditText)findViewById(R.id.edtSo2);
+                //b1.1 lấy data từ 2 điều khiển trên
+                String strso1 =so1.getText().toString();
+                String strso2 =so2.getText().toString();
+                //b1.3 chuyển data từ chuỗi sang số
+                float fso1 = Float.parseFloat(strso1);
+                float fso2 = Float.parseFloat(strso2);
+
+                //b2 tính toán
+                float Thuong = fso1 / fso2;
+                //b3 hiện kết quả
+                // tìm data
+//        EditText kqua = (EditText)findViewById(R.id.edtKQ);
+                // cbi data xuất, biến thành dạng chuỗi
+                String strkqua = String.valueOf(Thuong);
+                //gắn kqua lên đk
+                kqua.setText(strkqua);
+            }
+        });
+    }
+
+}
