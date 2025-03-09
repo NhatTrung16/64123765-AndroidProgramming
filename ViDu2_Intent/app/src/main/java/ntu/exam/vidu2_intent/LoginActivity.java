6447bb017ccabb438a1d7432dff4fd8391bcf802
Nhật Trung Nguyễn <kenkenhallo16@gmail.com>
2025-03-09 09:09:34 +0700

    Tạo xong intent cho nút Đăng nhập

--- ViDu2_Intent/app/src/main/java/ntu/exam/vidu2_intent/LoginActivity.java
+++ ViDu2_Intent/app/src/main/java/ntu/exam/vidu2_intent/LoginActivity.java
@@ -1,9 +1,6 @@
 package ntu.exam.vidu2_intent;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,29 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
-    Button nutDN;
-    void TimDieuKhien(){
-        nutDN = (Button) findViewById(R.id.btnLogin);
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        TimDieuKhien();
-        nutDN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent iLogin = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(iLogin);
-            }
         });
     }
 }
