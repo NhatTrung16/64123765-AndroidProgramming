6fab518170e33161a75cd955506f072939aa4f3c
Nhật Trung Nguyễn <kenkenhallo16@gmail.com>
2025-03-12 09:56:21 +0700

    hoàn thành Intent

--- Ex6_IntentDonGian/app/src/main/java/ntu/exam/ex6_intentdongian/MainActivity.java
+++ Ex6_IntentDonGian/app/src/main/java/ntu/exam/ex6_intentdongian/MainActivity.java
@@ -1,6 +1,9 @@
 package ntu.exam.ex6_intentdongian;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button nutChuyen1;
+    Button nutChuyen2;
+    void TimDieuKhien(){
+        nutChuyen1 = (Button) findViewById(R.id.btnMH1);
+        nutChuyen2 = (Button) findViewById(R.id.btnMH2);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,21 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        TimDieuKhien();
+        nutChuyen1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMH2 = new Intent(MainActivity.this, MH2Activity.class);
+                startActivity(intentMH2);
+            }
+        });
+        nutChuyen2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMH3 = new Intent(MainActivity.this, MH3Activity.class);
+                startActivity(intentMH3);
+            }
         });
     }
 }
