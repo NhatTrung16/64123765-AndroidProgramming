e1b8b0741b618a5217bc7dd34e8a4c913ddf0372
Nhật Trung Nguyễn <kenkenhallo16@gmail.com>
2025-03-12 10:29:01 +0700

    xử lý Intent màn hình chính

--- Ex7_IntentLogin/app/src/main/java/ntu/exam/ex7_intentlogin/MainActivity.java
+++ Ex7_IntentLogin/app/src/main/java/ntu/exam/ex7_intentlogin/MainActivity.java
@@ -1,6 +1,9 @@
 package ntu.exam.ex7_intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button nutDN;
+    void TimDieuKhien(){
+        nutDN = (Button) findViewById(R.id.btnLogin);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        TimDieuKhien();
+        nutDN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iLogin = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(iLogin);
+            }
         });
     }
 }
