9ee13eb7b6d47c20fcdd2c36c64c3a34296f01e7
Nhật Trung Nguyễn <kenkenhallo16@gmail.com>
2025-03-17 10:37:29 +0700

    hoàn thiện code

--- ModernDashboard/app/src/main/java/ntu/exam/moderndashboard/MainActivity.java
+++ ModernDashboard/app/src/main/java/ntu/exam/moderndashboard/MainActivity.java
@@ -1,20 +1,31 @@
 package ntu.exam.moderndashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    CardView clothingCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        clothingCard = (CardView) findViewById(R.id.clothingCard);
 
+        clothingCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iCloth = new Intent(MainActivity.this, ClothingActivity.class);
+                startActivity(iCloth);
+            }
+        });
     }
 }
