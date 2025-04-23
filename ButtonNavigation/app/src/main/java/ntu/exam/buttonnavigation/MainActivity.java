10f6ef9f536011ade5730fddd80e698a2bf33826
Nhật Trung Nguyễn <kenkenhallo16@gmail.com>
2025-04-23 16:57:21 +0700

    chuyển frag

--- ButtonNavigation/app/src/main/java/ntu/exam/buttonnavigation/MainActivity.java
+++ ButtonNavigation/app/src/main/java/ntu/exam/buttonnavigation/MainActivity.java
@@ -1,14 +1,24 @@
 package ntu.exam.buttonnavigation;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNav;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,37 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        bottomNav = findViewById(R.id.bot_nav);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int mnuItemDuocChonID = item.getItemId();
+                if (mnuItemDuocChonID == R.id.mnu_home){
+                   fragmentManager.beginTransaction()
+                           .replace(R.id.fragmentContainerView, new HomeFragment())
+                           .commit();
+                    return true;
+
+                }
+                if (mnuItemDuocChonID == R.id.mnu_search){
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.fragmentContainerView, new SearchFragment())
+                            .commit();
+                    return true;
+
+                }
+
+                if (mnuItemDuocChonID == R.id.mnu_profile){
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.fragmentContainerView, new ProfileFragment())
+                            .commit();
+                    return true;
+
+                }
+                return false;
+            }
         });
     }
 }
